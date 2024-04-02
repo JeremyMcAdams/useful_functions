0936f29ce9ea1c725e829d60efa348c0131813d6
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-04-01 23:48:51 -0400

    "Created a basic header file for type conversion"

---
+++ strnum.h
@@ -0,0 +1,7 @@
+#ifndef STRNUM_H
+#define STRNUM_H
+int string_to_int(char string[]);
+unsigned int string_to_uint(char string[]);
+float string_to_float(char string[]);
+double string_to_double(char string[]);
+#endif
