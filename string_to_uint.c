a2598a8f3f74403d8dc27f43e4146f516780de80
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-04-03 18:39:31 -0400

    Removed blocks that are rendered useless

--- string_to_uint.c
+++ string_to_uint.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "strnum.h"
+#include <time.h>
+long int power_10(const int num, const int exponent){
+    const int base = 10;
+    long int number = 1;
+    for (int i = 0; i < exponent; i++){
+        number *= base;
+    }
+    number *= num;
+    return number;
+}
 
 unsigned int string_to_uint(char string[]){
     char* string_pointer;
@@ -10,45 +20,47 @@ unsigned int string_to_uint(char string[]){
         printf("Error! Input is too long to convert\n");
         return -1;
     }
-    int buffer_size = str_len;
     int *num_array = malloc(sizeof(int) * str_len);
     if (!num_array){
         free(num_array);
         printf("Errno -1: Memory allocation failed");
         return -1;
     }
-    int i = 0;
+    
+    int counter = 0;
     for (char* character = string; *character; ++character){
-        if (i == buffer_size){
-            buffer_size += 1;
-            num_array = realloc(num_array, buffer_size * sizeof(int));
-            if (!num_array){
-                free(num_array);
-                printf("Errno -1: Memory reallocation failure\n");
-                return -1;
-            }
-        }
         if (*character >='0' && *character <= '9'){
-            num_array[i] = *character - 48;
+            num_array[counter] = *character - 48;
         }
         else {
-            num_array[i] = -1;
+            num_array[counter] = -1;
         }
         
-        if (num_array[i] == -1){
+        if (num_array[counter] == -1){
             printf("Errno -2: Non-number dectected\n");
             free(num_array);
             return -2;
         }
-        i++;
+        counter++;
     }
     int iter = 0;
     int number = 0;
-    for (int i = buffer_size -1 ; i >= 0; i--){
+    for (int i = str_len -1 ; i >= 0; i--){
         number += power_10(num_array[iter], i); 
         iter++;
     }
 
     free(num_array);
     return number;
+}
+
+int main(){
+    char string[] = "12354329";
+    clock_t start, stop;
+    start = clock();
+    int number = string_to_uint(string);
+    stop = clock();
+    printf("%d\n", number);
+    printf("Function took %lf sec\n", (stop - start)/ (double) CLOCKS_PER_SEC);
+    return 0;
 }
