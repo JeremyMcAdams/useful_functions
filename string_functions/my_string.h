ba595d8ec9d4e696d60a72f4546e4bb7756c987a
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-04-05 22:57:02 -0400

    Added header file

---
+++ string_functions/my_string.h
@@ -0,0 +1,6 @@
+#ifndef MY_STRING_H
+#define MY_STRING_H
+void my_strncpy(char destination[], char* source, const int buffer_length);
+int my_strlen(char* string);
+void my_safe_gets(char* destination, int buffer_size);
+#endif
