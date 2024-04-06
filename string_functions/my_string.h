effa9029a92297295c99a7ffe231e1a12329a68f
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-04-05 22:59:58 -0400

    Changed function names in file

--- string_functions/my_string.h
+++ string_functions/my_string.h
@@ -1,6 +1,6 @@
 #ifndef MY_STRING_H
 #define MY_STRING_H
-void my_strncpy(char destination[], char* source, const int buffer_length);
-int my_strlen(char* string);
-void my_safe_gets(char* destination, int buffer_size);
+void new_strncpy(char destination[], char* source, const int buffer_length);
+int new_strlen(char* string);
+void safe_gets(char* destination, int buffer_size);
 #endif
