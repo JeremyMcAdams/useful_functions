c510934675216034164fcc134b996c77220cb3c0
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-04-28 11:16:57 -0400

    Removed unused variable

--- string_functions/new_strncpy.c
+++ string_functions/new_strncpy.c
@@ -1,12 +1,7 @@
 #include "my_string.h"
 #include <stdio.h>
-#define bool int
-#define true 1
-#define false 0
 void new_strncpy(register char* destination, register char* source, register int buffer_length){
-    bool source_end = false;
     for (register int i = 0; i <= buffer_length; ++i){
-    
         if (source[i] == '\0'){
             while (i < buffer_length){
                 destination[i] = '\0';
