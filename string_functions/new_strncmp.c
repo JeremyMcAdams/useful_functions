e9ef88921f93d4aac19b730c832b7dafe7b4bcc3
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-04-28 11:18:19 -0400

    Added register keyword to maybe improve speed

--- string_functions/new_strncmp.c
+++ string_functions/new_strncmp.c
@@ -1,6 +1,6 @@
 #include "my_string.h"
 int new_strncmp(register const char* string, register const char* comp_string, register const int len){
-	for (int i = 0; i < len; ++i){
+	for (register int i = 0; i < len; ++i){
 		if (*string != *comp_string){
 			return (*string - *comp_string);
 		}
