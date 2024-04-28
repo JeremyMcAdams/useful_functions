4aa3c387d545232b9f52847962ef9dfd44142d22
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-04-28 11:10:43 -0400

    Ghost letter bug appears to be fixed

--- string_functions/new_strncpy.c
+++ string_functions/new_strncpy.c
@@ -1,22 +1,28 @@
 #include "my_string.h"
+#include <stdio.h>
 #define bool int
 #define true 1
 #define false 0
 void new_strncpy(register char* destination, register char* source, register int buffer_length){
     bool source_end = false;
-    for (int i = 0; i <= buffer_length; ++i){
+    for (register int i = 0; i <= buffer_length; ++i){
+    
         if (source[i] == '\0'){
-            source_end = true;
+            while (i < buffer_length){
+                destination[i] = '\0';
+                i += 1;
+            }
+            break;
         }
         else if (i == buffer_length){
             destination[i] = '\0';
         }
-        else if (source_end){
-            destination[i] = '\0';
-        }
         else {
-            destination[i] = source[i]; 
-        }   
+            destination[i] = source[i];
+        }
+    }
+    if (buffer_length == 0){
+        destination[0] = '\0';
     }
 }
 
