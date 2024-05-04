fcbd0618f474dfd54903ab8244eac722ae96b61b
Jeremy McAdams <JeremyMcAdams91@gmail.com>
2024-05-04 00:01:08 -0400

    Updated to add new functions

--- string_functions/my_string.h
+++ string_functions/my_string.h
@@ -1,10 +1,13 @@
 #ifndef MY_STRING_H
 #define MY_STRING_H
-void new_strncpy(register char* destination, register char* source, register int buffer_length);
-int new_strlen(char* string);
-void safe_gets(char* destination, int buffer_size);
-char* new_strstr(char* string, char* substring);
+void new_strncpy(register char* destination, register char* source, register const unsigned int buffer_length);
+int new_strlen(register char* string);
+void safe_gets(register char* destination, register int buffer_size);
+char* new_strstr(register char* string, register char* substring);
 int new_strcmp(register const char* string, register const char* comp_string);
 int new_strncmp(register const char* string, register const char* comp_string, register const int len);
-void strip_string(char* string, char* delimiters);
+void strip_string(register char* string, register char* delimiters);
+void upper(register char* string);
+void lower(register char* string);
+char* new_strtok(register char* string, register char* delimiters);
 #endif
